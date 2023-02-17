cdd126325c73394820c5df1afa489fc65b4e2a33
Angry Fool <93109413+lrutjens@users.noreply.github.com>
2023-02-17 11:25:12 -0600

    spawnship test

--- Melon Mod/Mod.cs
+++ Melon Mod/Mod.cs
@@ -8,10 +8,6 @@ namespace DebugMod
 {
     public class TestMod : MelonMod
     {
-        bool hasUpdateStarted = true;
-        bool hasFixedUpdateStarted = true;
-        bool hasLateUpdateStarted = true;
-
         public void LoadAssetBundle()
         {
             //Load the assetBundle
@@ -41,33 +37,14 @@ namespace DebugMod
 
         public override void OnUpdate()
         {
-            if(hasUpdateStarted == true)
+            if (Input.GetKeyDown(KeyCode.F1))
             {
-                MelonLogger.Msg("If you see this, that means the update function is working");
-                hasUpdateStarted = false;
-            }
-        }
-        
-        public override void OnFixedUpdate()
-        {
-            if(hasFixedUpdateStarted == true)
-            {
-                MelonLogger.Msg("If you see this, that means OnFixedUpdate works");
-                hasFixedUpdateStarted = false;
-            }
-        
-            /*bool triggerValue;
-            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
-            {
-                MelonLogger.Msg("Trigger button is pressed.");
-            }*/
-        }        
-        public override void OnLateUpdate()
-        {
-            if(hasLateUpdateStarted == true)
-            {
-                MelonLogger.Msg("If you see this, that means OnLateUpdate works, this message should appear after all the others, with the except being possibly OnFixedUpdate");
-                hasLateUpdateStarted = false;
+                while (i < 10)
+                {
+                    TeamManager.instance.GetTeam(1).TryProduceShip();
+                    i++;
+                }
+                i = 0;
             }
         }
     }
